b28c77b9c2d6d631b3ad0a80038686c0c9e18284
mpoapostolis <mpoapostolis@gmail.com>
2020-12-02 21:25:49 +0200

    change netlify proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
-const target = 'http://localhost:3001';
-// const target = 'https://marines-serveless.vercel.app';
+// const target = 'http://localhost:3001';
+const target = 'https://marines-serveless.vercel.app';
 
 module.exports = function (app) {
   app.use(
